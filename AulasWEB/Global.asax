5fd5b327787cb73b56a6e19a76cfe65f46db24cd
DJALMA MUSETTI JR <djasistemas@gmail.com>
2022-08-25 12:03:18 -0300

    Add project files.

---
+++ AulasWEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AulasWEB.MvcApplication" Language="C#" %>
---
+++ AulasWEB/Models/Conexao.cs
@@ -0,0 +1,44 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AulasWEB.Models
+{
+   public class Conexao : IDisposable
+   {
+      public MySqlConnection conn;
+      private readonly string host = "";
+      private readonly string port = "";
+      private readonly string db = "";
+      private readonly string user = "";
+      private readonly string pass = "";
+
+      private void Conectar()
+      {
+         string StrConn = "";
+         StrConn = "Server=" + host + "; Port=" + port + "; Database=" + db + "; Uid=" + user + "; Pwd=" + pass + ";  ";
+         conn = new MySqlConnection(StrConn);
+         try
+         {
+            conn.Open();
+         }
+         catch (Exception)
+         {
+            throw;
+         }
+      }
+
+      public Conexao()
+      {
+         Conectar();
+      }
+
+      public void Dispose()
+      {
+         conn.Close();
+         conn.Dispose();
+      }
+   }
+}
